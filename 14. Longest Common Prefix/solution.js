c16590121dda193b9d6cc5b50000908e05edbc30
GD DEV <gdavid.ptorrez@gmail.com>
2020-07-18 17:13:08 -0400

    IMP: Add readme main

---
+++ 14. Longest Common Prefix/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {};
+
+console.log(
+  '["flower","flow","flight"]',
+  longestCommonPrefix(["flower", "flow", "flight"])
+);
+
+console.log(
+  '["flower","flow","flight"]',
+  longestCommonPrefix(["dog", "racecar", "car"])
+);
