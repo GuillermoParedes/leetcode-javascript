10582c369f06e98fc4172ddaf1b2b874af94f7d6
GD DEV <gdavid.ptorrez@gmail.com>
2020-07-20 22:22:02 -0400

    IMP: solution 20 problem

--- 20. Valid Parentheses/solution.js
+++ 20. Valid Parentheses/solution.js
@@ -3,38 +3,34 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-  const signes = new Map();
-  signes.set("(", ")");
-  signes.set("[", "]");
-  signes.set("{", "}");
-  if (s.length % 2 === 0) {
-    let aux = s;
-    let flag = true;
-    let end = s.length - 1;
-    let start = 0;
-    let limit = s.length / 2;
+  console.log("type", typeof s);
+  const signesMap = new Map();
+  signesMap.set("(", ")");
+  signesMap.set("{", "}");
+  signesMap.set("[", "]");
 
-    while (limit >= start && limit <= end) {
-      if (signes.get(aux.charAt(start)) !== aux.charAt(end)) {
-        // if (signes.get(aux.charAt(start) !== aux.charAt(start + 1))) {
-        flag = false;
-        // }
+  const stack = [];
+
+  for (const paran of s) {
+    if (signesMap.get(paran)) {
+      stack.push(paran);
+    } else {
+      const closeParan = stack.pop();
+      if (paran !== signesMap.get(closeParan)) {
+        return false;
       }
-      start++;
-      end--;
     }
-    return flag;
-  } else {
-    return false;
   }
-};
 
-console.log('"()"::::>', isValid("()"));
-console.log("================EXPECTED: TRUE=======================");
-console.log('"()[]{}"::::>', isValid("()[]{}"));
-console.log("================EXPECTED: TRUE=======================");
-console.log('"(]"::::>', isValid("(]"));
-console.log("================EXPECTED: TRUE=======================");
-console.log('"([)]"::::>', isValid("([)]"));
-console.log("================EXPECTED: TRUE=======================");
+  return stack.length === 0;
+};
+// console.log('"()"::::>', isValid("()"));
+
+// console.log('"(][){}"::::>', isValid("(][){}"));
+
+// console.log('"(]"::::>', isValid("(]"));
+
+// console.log('"([)]"::::>', isValid("([)]"));
+
 console.log('"{[]}"::::>', isValid("{[]}"));
+console.log('"[{())}]"::::>', isValid("[{())}]"));
